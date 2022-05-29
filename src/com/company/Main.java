f1f7894fa3ed2575dc75407457130f5917d174e5
Hilary Natalie Tabra <Misspurple@hilarys-air.home>
2022-05-29 19:03:18 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+
+package task2;
+
+class Main {
+    public static void main(String[] args) {
+        try {
+            ThrowError.Error();
+        } catch (ArrayIndexOutOfBoundsException arr) {
+            arr.getMessage();
+        } finally {
+            System.out.println("Finally block has been run already");
+        }
+    }
+}
+
+public class ThrowError {
+    public static void Error() {
+        String[] arr = new String[3];
+        for (int i = 0; i < 6; i++) {
+            arr[i] = arr[i];
+        }
+    }
+}
